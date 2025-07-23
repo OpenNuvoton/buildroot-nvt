4ecb2821bbb0d39d47e78e883e3b41630a67f928
schung <schung@nuvoton.com>
2025-07-23 11:22:54 +0800

    Add linux-6.6.y on ma35 series platforms
    Change-Id: I4b38bd0c7df48434f194bc0baa1416645e56a8e7

--- linux/linux.mk
+++ linux/linux.mk
@@ -29,6 +29,10 @@ else ifeq ($(BR2_LINUX_KERNEL_MA35_5_10_VERSION),y)
 LINUX_TARBALL = $(call github,OpenNuvoton,MA35D1_linux-5.10.y,$(call qstrip,$(BR2_TARGET_KERNEL_MA35_VERSION)))/MA35D1_linux-5.10.y-$(call qstrip,$(BR2_TARGET_KERNEL_MA35_VERSION)).tar.gz
 LINUX_SITE = $(patsubst %/,%,$(dir $(LINUX_TARBALL)))
 LINUX_SOURCE = $(notdir $(LINUX_TARBALL))
+else ifeq ($(BR2_LINUX_KERNEL_MA35_6_6_VERSION),y)
+LINUX_TARBALL = $(call github,OpenNuvoton,MA35D1_linux-6.6.y,$(call qstrip,$(BR2_TARGET_KERNEL_MA35_VERSION)))/MA35D1_linux-6.6.y-$(call qstrip,$(BR2_TARGET_KERNEL_MA35_VERSION)).tar.gz
+LINUX_SITE = $(patsubst %/,%/,$(dir $(LINUX_TARBALL)))
+LINUX_SOURCE = $(notdir $(LINUX_TARBALL))
 else ifeq ($(BR2_LINUX_KERNEL_CUSTOM_GIT),y)
 LINUX_SITE = $(call qstrip,$(BR2_LINUX_KERNEL_CUSTOM_REPO_URL))
 LINUX_SITE_METHOD = git
