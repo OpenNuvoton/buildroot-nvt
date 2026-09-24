3885e75827471b801009f93493c122eae6103f93
zychen <zychen@nuvoton.com>
2026-09-24 14:08:57 +0800

    linux: fix MA35D1 6.18 kernel source path
    Add the missing kernel and kernel headers download paths for
    the MA35D1 Linux 6.18.y kernel series.
    
    Change-Id: Ibe5c27651b021b7b7b4e3e74608f587ec38a898f

--- linux/linux.mk
+++ linux/linux.mk
@@ -53,6 +53,10 @@ else ifeq ($(BR2_LINUX_KERNEL_MA35_6_12_VERSION),y)
 LINUX_TARBALL = $(call github,OpenNuvoton,MA35D1_linux-6.12.y-cip,$(call qstrip,$(BR2_TARGET_KERNEL_MA35_VERSION)))/MA35D1_linux-6.12.y-cip-$(call qstrip,$(BR2_TARGET_KERNEL_MA35_VERSION)).tar.gz
 LINUX_SITE = $(patsubst %/,%,$(dir $(LINUX_TARBALL)))
 LINUX_SOURCE = $(notdir $(LINUX_TARBALL))
+else ifeq ($(BR2_LINUX_KERNEL_MA35_6_18_VERSION),y)
+LINUX_TARBALL = $(call github,OpenNuvoton,MA35D1_linux-6.18.y,$(call qstrip,$(BR2_TARGET_KERNEL_MA35_VERSION)))/MA35D1_linux-6.18.y-$(call qstrip,$(BR2_TARGET_KERNEL_MA35_VERSION)).tar.gz
+LINUX_SITE = $(patsubst %/,%,$(dir $(LINUX_TARBALL)))
+LINUX_SOURCE = $(notdir $(LINUX_TARBALL))
 else ifeq ($(BR2_LINUX_KERNEL_CUSTOM_GIT),y)
 LINUX_SITE = $(call qstrip,$(BR2_LINUX_KERNEL_CUSTOM_REPO_URL))
 LINUX_SITE_METHOD = git
